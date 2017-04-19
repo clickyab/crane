3ffcdcdbffcfb92f53350e0d50f7adcab6d9f263
Kian Ostad <c@kianostad.com>
2017-04-19 16:55:46 +0430

    Add test for rtb
    fix 1308

--- src/rtb/select.go
+++ src/rtb/select.go
@@ -8,9 +8,11 @@ import (
 // SelectCPM is the simplest way to bid. sort the value, return the
 func SelectCPM(imp entity.Impression, all map[string][]entity.Advertise) map[string]entity.Advertise {
 	res := make(map[string]entity.Advertise, len(all))
+
 	for id := range all {
 		if len(all[id]) == 0 {
 			res[id] = nil
+			continue
 		}
 		sorted := sortedAd(all[id])
 		sort.Sort(sorted)
--- src/rtb/sort.go
+++ src/rtb/sort.go
@@ -11,7 +11,7 @@ func (sa sortedAd) Len() int {
 func (sa sortedAd) Less(i, j int) bool {
 	cpi := sa[i].MaxCPM() * sa[i].Demand().Handicap()
 	cpj := sa[j].MaxCPM() * sa[j].Demand().Handicap()
-	return cpi < cpj
+	return cpi > cpj
 }
 
 func (sa sortedAd) Swap(i, j int) {
