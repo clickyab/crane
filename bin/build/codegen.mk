254e134f395f59fd83652d7f32b88b1bc8be68b0
danicheeta <danicheeta@yahoo.com>
2017-06-24 16:34:40 +0430

    makefile fixed, had wrong codegen directory

--- bin/build/codegen.mk
+++ bin/build/codegen.mk
@@ -1,11 +1,10 @@
 tools-codegen:
-	$(BUILD) clickyab.com/exchange/commands/codegen
+	$(BUILD) clickyab.com/crane/commands/codegen
 
 octopus-user: tools-codegen
-	$(BIN)/codegen -p clickyab.com/exchange/octopus/console/user/aaa
-	$(BIN)/codegen -p clickyab.com/exchange/octopus/console/user/routes
-
-octopus-report: tools-codegen
-	$(BIN)/codegen -p clickyab.com/exchange/octopus/console/report/routes
+	$(BIN)/codegen -p clickyab.com/crane/crane/models/internal/ad
+	$(BIN)/codegen -p clickyab.com/crane/crane/models/internal/campaign
+	$(BIN)/codegen -p clickyab.com/crane/crane/models/internal/user
+	$(BIN)/codegen -p clickyab.com/crane/crane/models/internal/publisher
 
 codegen: ip2location migration octopus-user
