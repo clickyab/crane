e41079f982df2a7c77cf3c8377ca80812285fd60
danicheeta <danicheeta@yahoo.com>
2017-06-24 17:07:23 +0430

    [WIP] interfaces changed and codegen fixed

--- crane/entity/slot.go
+++ crane/entity/slot.go
@@ -2,16 +2,14 @@ package entity
 
 // Slot is the slot of the app
 type Slot interface {
-	// ID of slot
-	ID() int64
-	// PublicID of slot
-	PublicID() string
-	// AllowedSize of slot, if the slot allowed multiple size, then the result is mor than one int
-	AllowedSize() []int
-	// Size return the primary size of this slot
-	Size() int
-	// StateID is an string for this slot, its a random at first but the value is not changed at all other calls
-	StateID() string
+	// TrackID of slot
+	TrackID() string
+	// Width return the primary size of this slot
+	Width() int
+	// Height return the primary size of this slot
+	Height() int
+	// SetSlotCTR the ctr from database
+	SetSlotCTR(float64)
 	// SlotCTR the ctr from database
 	SlotCTR() float64
 	// Set winner ad for this slot
