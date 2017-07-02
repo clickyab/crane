77eee00bb329d42efefb4f9fd7c039b1f405955c
danicheeta <danicheeta@yahoo.com>
2017-07-02 12:57:29 +0430

    vast output renderer implementation
    Fix #2471

---
+++ crane/layers/output/vast/render.go
@@ -0,0 +1,22 @@
+package vast
+
+import (
+	"io"
+
+	"clickyab.com/crane/crane/entity"
+)
+
+type data struct {
+	data []byte
+}
+
+// Render implementation
+func (v *data) Render(writer io.Writer, imp entity.Impression, cp entity.ClickProvider) error {
+	v.parse(imp, cp)
+	_, err := writer.Write(v.data)
+	return err
+}
+
+/*func init() {
+
+}*/
