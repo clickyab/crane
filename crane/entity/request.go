5759d9a496d9c3fa87c505412aaf1115fd68e5c2
Kian Ostad <c@kianostad.com>
2017-12-03 17:48:53 +0330

    Fix context interface fix #4359

--- crane/entity/request.go
+++ crane/entity/request.go
@@ -4,6 +4,16 @@ import (
 	"net"
 )
 
+// Protocol is the scheme of http url
+type Protocol string
+
+const (
+	// HTTP is scheme of request
+	HTTP Protocol = "http"
+	// HTTPS is scheme of request
+	HTTPS Protocol = "https"
+)
+
 // Request get the request from request
 type Request interface {
 	// IP get the real ip from the request
@@ -13,11 +23,9 @@ type Request interface {
 	// Client get the client key (cop)
 	ClientID() string
 	// Protocol return http or https
-	Protocol() string
+	Protocol() Protocol
 	// UserAgent user agent
 	UserAgent() string
 	// Location return the location
 	Location() Location
-	// Attributes get attributes of different type requests
-	Attributes() map[string]string
 }
