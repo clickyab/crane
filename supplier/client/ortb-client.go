06973b0ddadfe20f5629d2de694e02c642895662
fzerorubigd <fzerorubigd@gmail.com>
2017-12-23 14:17:45 +0330

    add openrtb caller
    fixes #4492

--- supplier/client/ortb-client.go
+++ supplier/client/ortb-client.go
@@ -1,1 +1,42 @@
 package client
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"net/http"
+
+	"github.com/bsm/openrtb"
+	"github.com/clickyab/services/xlog"
+)
+
+// Call an openrtp end point
+func Call(ctx context.Context, method, url string, pl *openrtb.BidRequest) (*openrtb.BidResponse, error) {
+	d, err := json.Marshal(pl)
+	if err != nil {
+		xlog.GetWithError(ctx, err).Debug("failed")
+		return nil, err
+	}
+	buf := bytes.NewReader(d)
+	r, err := http.NewRequest(method, url, buf)
+	if err != nil {
+		xlog.GetWithError(ctx, err).Debug("failed")
+		return nil, err
+	}
+	nCtx, _ := context.WithTimeout(ctx, timeout.Duration())
+	resp, err := httpClient.Do(r.WithContext(nCtx))
+	if err != nil {
+		xlog.GetWithError(ctx, err).Debug("failed")
+		return nil, err
+	}
+
+	bid := openrtb.BidResponse{}
+	err = json.NewDecoder(resp.Body).Decode(&bid)
+	if err != nil {
+		xlog.GetWithError(ctx, err).Debug("failed")
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	return &bid, nil
+}
