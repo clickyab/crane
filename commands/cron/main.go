7bb14e436e5dfbe35fdcc582a41f9b8b125d7f0e
dara <dara51php@gmail.com>
2017-12-23 22:00:27 +0330

    add cron to update daily report note : unique index should be applied first Fix #4486

---
+++ commands/cron/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"os"
+
+	"sync"
+
+	"context"
+
+	"flag"
+
+	"strconv"
+	"time"
+
+	"clickyab.com/crane/commands"
+	"clickyab.com/crane/models/cron"
+	"github.com/clickyab/services/assert"
+	"github.com/clickyab/services/config"
+	"github.com/clickyab/services/initializer"
+	_ "github.com/clickyab/services/mysql/connection/mysql"
+	"github.com/clickyab/services/safe"
+	"github.com/sirupsen/logrus"
+)
+
+var (
+	wg = &sync.WaitGroup{}
+)
+
+func main() {
+	config.Initialize(commands.Organization, commands.AppName, commands.Prefix, commands.DefaultConfig())
+	config.DumpConfig(os.Stdout)
+	defer initializer.Initialize()()
+
+	def, _ := strconv.Atoi(time.Now().Format("20060102"))
+	fDate := flag.Int("d", def, "date for cron job")
+	flag.Parse()
+
+	// if d=-1 the cron runs for yesterday
+	if *fDate == -1 {
+		yesterday, _ := strconv.Atoi(time.Now().AddDate(0, 0, -1).Format("20060102"))
+		*fDate = yesterday
+	}
+
+	ctx := context.Background()
+	wg.Add(4)
+
+	safe.GoRoutine(ctx, func() {
+		defer wg.Done()
+		assert.Nil(cron.WebImp(*fDate))
+	})
+	safe.GoRoutine(ctx, func() {
+		defer wg.Done()
+		assert.Nil(cron.WebClick(*fDate))
+	})
+	safe.GoRoutine(ctx, func() {
+		defer wg.Done()
+		assert.Nil(cron.AppImp(*fDate))
+	})
+	safe.GoRoutine(ctx, func() {
+		defer wg.Done()
+		assert.Nil(cron.AppClick(*fDate))
+	})
+
+	wg.Wait()
+
+	logrus.Debug("cron finished")
+
+}
