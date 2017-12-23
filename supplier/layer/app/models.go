dad537aa5fb21118ec175221e65c6cc33786f5a4
Kian Ostad <c@kianostad.com>
2017-12-23 16:17:36 +0330

    App get ad route
    fix #4494

---
+++ supplier/layer/app/models.go
@@ -0,0 +1,64 @@
+package app
+
+import (
+	"clickyab.com/crane/demand/entity"
+)
+
+type supplier struct {
+}
+
+func (s *supplier) Name() string {
+	return "clickyab"
+}
+
+func (s *supplier) Token() string {
+	panic("implement me")
+}
+
+func (s *supplier) DefaultFloorCPM() int64 {
+	panic("implement me")
+}
+
+func (s *supplier) DefaultSoftFloorCPM() int64 {
+	panic("implement me")
+}
+
+func (s *supplier) DefaultMinBid() int64 {
+	panic("implement me")
+}
+
+func (s *supplier) BidType() entity.BIDType {
+	panic("implement me")
+}
+
+func (s *supplier) DefaultCTR() float64 {
+	panic("implement me")
+}
+
+func (s *supplier) AllowCreate() bool {
+	return false
+}
+
+func (s *supplier) TinyMark() bool {
+	panic("implement me")
+}
+
+func (s *supplier) TinyLogo() string {
+	panic("implement me")
+}
+
+func (s *supplier) TinyURL() string {
+	panic("implement me")
+}
+
+func (s *supplier) ShowDomain() string {
+	panic("implement me")
+}
+
+func (s *supplier) UserID() int64 {
+	panic("implement me")
+}
+
+func (s *supplier) Rate() int {
+	panic("implement me")
+}
