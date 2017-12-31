02be414b3722c24b8ea9d5ecbc20234bbacb9615
Ehsan <zerob4wl@gmail.com>
2017-12-31 11:05:41 +0000

    set style for each element

--- js/src/show-ad.ts
+++ js/src/show-ad.ts
@@ -5,8 +5,8 @@ import FP from 'fingerprintjs2'
 
 import Effect from './effect'
 
-declare let clickyabParams: { [key: string]: string }
-declare let escape: any
+declare var clickyabParams: { [key: string]: string }
+declare var escape: any
 
 export default class ShowAd {
   private ads: IAd[] = []
@@ -22,98 +22,13 @@ export default class ShowAd {
     }
   }
 
-  isMobile(): number {
-    if (
-      navigator.userAgent.match(/Android/i) ||
-      navigator.userAgent.match(/webOS/i) ||
-      navigator.userAgent.match(/iPhone/i) ||
-      navigator.userAgent.match(/iPad/i) ||
-      navigator.userAgent.match(/iPod/i) ||
-      navigator.userAgent.match(/BlackBerry/i) ||
-      navigator.userAgent.match(/Windows Phone/i)
-    ) {
-      return 1
-    } else {
-      return 0
-    }
-  }
-
-  checkSlotId(elements: Element[], element: Element): any {
-    return new Promise(res => {
-      if (
-        elements.findIndex(
-          e => e.getAttribute('clickyab-slot') === element.getAttribute('clickyab-slot')
-        ) !== -1
-      ) {
-        element.setAttribute('clickyab-slot', element.getAttribute('clickyab-slot') + '1')
-        return this.checkSlotId(elements, element)
-      } else {
-        res()
-      }
-    })
-  }
-
-  findAdsInPage(): Element[] {
-    let elements: Element[] = []
-    const elementsCollection = document.getElementsByClassName(CONFIG.SELECTOR_CLASS)
-    for (let i = 0; i < elementsCollection.length; i++) {
-      this.checkSlotId(elements, elementsCollection.item(i))
-      elements.push(elementsCollection.item(i))
-    }
-    return elements
-  }
-
-  injectMobileAds(src: string) {
-    const div = document.createElement('div')
-    div.setAttribute(
-      'style',
-      `position: fixed; width: 100%; z-index:99999999; left: 0; bottom: 0px; margin: 0; padding: 0; text-align: center;`
-    )
-    div.innerHTML = decodeURIComponent(ad.iframe || '')
-
-    document.getElementsByTagName('body')[0].appendChild(div)
-  }
-
-  getAdSize(ad: IAd): number {
-    const size: string = `${ad.width}_${ad.height}`
-    const sizes: { [index: string]: number } = CONFIG.BANNER_SIZES as { [index: string]: number }
-    return sizes[size] || -1
-  }
-
-  encodeuri(b: string): string {
-    if (typeof encodeURIComponent === 'function') {
-      return encodeURIComponent(b)
-    } else {
-      return escape(b)
-    }
-  }
-
-  setCookie(cname: string, cvalue: string, exdays: number) {
-    const d = new Date()
-    d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000)
-    const expires = 'expires=' + d.toUTCString()
-    document.cookie = cname + '=' + cvalue + '; ' + expires
-  }
-
-  getCookie(cname: string): string {
-    const name = cname + '='
-    const ca = document.cookie.split(';')
-    for (let i = 0; i < ca.length; i++) {
-      let c = ca[i]
-      while (c.charAt(0) === ' ') {
-        c = c.substring(1)
-      }
-      if (c.indexOf(name) === 0) {
-        return c.substring(name.length, c.length)
-      }
-    }
-    return ''
-  }
-
   public run() {
     if (window.document.body.getAttribute('clickyab-showAd-ready') === 'true') return
     window.document.body.setAttribute('clickyab-showAd-ready', 'true')
-    this.ads = this.findAdsInPage().map(elem => this.parseElementProps(elem))
+    console.log('start show ad')
+    this.ads = this.findAdsInPage()
+      .map(elem => this.parseElementProps(elem))
+      .map(this.setStyle)
     this.getAdsFromRemote(ads => {
       this.injectSrc(ads)
       this.injectIframes()
@@ -121,6 +36,13 @@ export default class ShowAd {
         this.injectMobileAds(ads.m)
       }
     })
+  }
+
+  private setStyle(ad: IAd) {
+    ad.element.style.width = ad.width + 'px'
+    ad.element.style.height = ad.height + 'px'
+    ad.element.style.textAlign = 'center'
+    return ad
   }
 
   private injectIframes() {
@@ -138,7 +60,6 @@ export default class ShowAd {
 
           if (ad.effect) {
             const effectAct = new Effect(ad)
-            // delete effectAct;
           }
         }
       }
@@ -197,7 +118,32 @@ export default class ShowAd {
     })
   }
 
-  private parseElementProps(element: Element): IAd {
+  private isMobile(): number {
+    if (
+      navigator.userAgent.match(/Android/i) ||
+      navigator.userAgent.match(/webOS/i) ||
+      navigator.userAgent.match(/iPhone/i) ||
+      navigator.userAgent.match(/iPad/i) ||
+      navigator.userAgent.match(/iPod/i) ||
+      navigator.userAgent.match(/BlackBerry/i) ||
+      navigator.userAgent.match(/Windows Phone/i)
+    ) {
+      return 1
+    } else {
+      return 0
+    }
+  }
+
+  private findAdsInPage(): HTMLElement[] {
+    let elements: HTMLElement[] = []
+    const elementsCollection = document.getElementsByClassName(CONFIG.SELECTOR_CLASS)
+    for (let i = 0; i < elementsCollection.length; i++) {
+      elements.push(elementsCollection.item(i) as HTMLElement)
+    }
+    return elements
+  }
+
+  private parseElementProps(element: HTMLElement): IAd {
     let ad: IAd = {
       element: element,
       slot: element.getAttribute(CONFIG.ELEMENT_PROPERTY_PREFIX + 'slot'),
@@ -213,5 +159,69 @@ export default class ShowAd {
 
   private validateAdElement(ad: IAd): boolean {
     return this.getAdSize(ad) !== -1
+  }
+
+  private injectMobileAds(src: string) {
+    const div = document.createElement('div')
+    div.setAttribute(
+      'style',
+      `position: fixed; width: 100%; z-index:99999999; left: 0; bottom: 0px; margin: 0; padding: 0; text-align: center;`
+    )
+    const template: string = `
+              <iframe name="clickyab_ads_frame_m"
+                 style="max-width: 100%;
+                        display: block;
+                        margin: 0 auto;"
+                 width=320
+                 height=50 
+                 frameborder=0 
+                 src="${src}"
+                 marginwidth="0" 
+                 marginheight="0" 
+                 vspace="0" 
+                 hspace="0" 
+                 allowtransparency="true" 
+                 scrolling="no">
+              </iframe>
+            </div>`
+    div.innerHTML = template
+
+    document.getElementsByTagName('body')[0].appendChild(div)
+  }
+
+  private getAdSize(ad: IAd): number {
+    const size: string = `${ad.width}_${ad.height}`
+    const sizes: { [index: string]: number } = CONFIG.BANNER_SIZES as { [index: string]: number }
+    return sizes[size] || -1
+  }
+
+  private encodeuri(b: string): string {
+    if (typeof encodeURIComponent === 'function') {
+      return encodeURIComponent(b)
+    } else {
+      return escape(b)
+    }
+  }
+
+  private setCookie(cname: string, cvalue: string, exdays: number) {
+    const d = new Date()
+    d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000)
+    const expires = 'expires=' + d.toUTCString()
+    document.cookie = cname + '=' + cvalue + '; ' + expires
+  }
+
+  private getCookie(cname: string): string {
+    const name = cname + '='
+    const ca = document.cookie.split(';')
+    for (let i = 0; i < ca.length; i++) {
+      let c = ca[i]
+      while (c.charAt(0) === ' ') {
+        c = c.substring(1)
+      }
+      if (c.indexOf(name) === 0) {
+        return c.substring(name.length, c.length)
+      }
+    }
+    return ''
   }
 }
