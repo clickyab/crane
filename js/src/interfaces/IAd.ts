bb8954454e57bbabbcc00807c11dd1294788ef29
Ehsan <zerob4wl@gmail.com>
2017-12-31 11:05:41 +0000

    set style for each element

--- js/src/interfaces/IAd.ts
+++ js/src/interfaces/IAd.ts
@@ -1,13 +1,13 @@
 export default interface IAd {
-  element: Element,
-  slot: string | null,
-  width: string | null,
-  height: string | null,
-  adType?: string | null,
-  minFlex?: string | null,
-  size?: number;
-  valid?: boolean;
-  src?: string;
-  effect?: string | null;
-  iframe?: string;
+  element: HTMLElement
+  slot: string | null
+  width: string | null
+  height: string | null
+  adType?: string | null
+  minFlex?: string | null
+  size?: number
+  valid?: boolean
+  src?: string
+  effect?: string | null
+  iframe?: string
 }
