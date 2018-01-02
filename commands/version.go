cf521ec4fb8d6005c56be22dba0157433dd3b70d
fzerorubigd <fzerorubigd@gmail.com>
2018-01-02 15:07:35 +0330

    add version module

---
+++ commands/version.go
@@ -0,0 +1,33 @@
+package commands
+
+import (
+	"context"
+	"net/http"
+
+	"fmt"
+
+	"github.com/clickyab/services/framework"
+	"github.com/clickyab/services/framework/router"
+	"github.com/clickyab/services/version"
+)
+
+type middleware struct {
+	data string
+}
+
+func (middleware) PreRoute() bool {
+	return true
+}
+
+func (m *middleware) Handler(next framework.Handler) framework.Handler {
+	return func(ctx context.Context, w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Cy-Version", m.data)
+		next(ctx, w, r)
+	}
+}
+
+func init() {
+	v := version.GetVersion()
+	data := fmt.Sprintf("%s:%d", v.Short, v.Count)
+	router.RegisterGlobalMiddleware(&middleware{data: data})
+}
