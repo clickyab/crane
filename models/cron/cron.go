c4d30557f07bb54ce66aa4b07d7c088c9bb25f52
Kian Ostad <c@kianostad.com>
2018-01-02 14:13:50 +0330

    Add log to crons

--- models/cron/cron.go
+++ models/cron/cron.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"clickyab.com/crane/models/internal/entities"
+	"github.com/sirupsen/logrus"
 )
 
 type impRes struct {
@@ -22,6 +23,10 @@ type clickRes struct {
 
 // WebImp select from impression type web
 func WebImp(date int) error {
+	logrus.Debug("start web impression worker")
+	defer func() {
+		logrus.Debug("web impression worker done")
+	}()
 	var webImps []impRes
 	q := fmt.Sprintf(`SELECT
 		COUNT(imp_id) AS imps,
@@ -38,9 +43,11 @@ func WebImp(date int) error {
 	if err != nil {
 		return err
 	}
+	logrus.Debugf("total web impression: %d", len(webImps))
+	wr := entities.NewManager().GetWDbMap()
 	for i := range webImps {
 		q := "INSERT INTO daily_report (supplier,type,publisher,imps,cpm,date) VALUES (?,?,?,?,?,?) ON DUPLICATE KEY UPDATE imps=VALUES(imps),cpm=VALUES(cpm)"
-		_, err := entities.NewManager().GetWDbMap().Exec(q, webImps[i].Supplier, "web", webImps[i].Publisher, webImps[i].Imps, webImps[i].CPM, date)
+		_, err := wr.Exec(q, webImps[i].Supplier, "web", webImps[i].Publisher, webImps[i].Imps, webImps[i].CPM, date)
 		if err != nil {
 			return err
 		}
@@ -50,6 +57,10 @@ func WebImp(date int) error {
 
 // AppImp select from impression type app
 func AppImp(date int) error {
+	logrus.Debug("start app impression worker")
+	defer func() {
+		logrus.Debug("web app worker done")
+	}()
 	var appImps []impRes
 	q := fmt.Sprintf(`SELECT
 		COUNT(imp_id) AS imps,
@@ -66,9 +77,12 @@ func AppImp(date int) error {
 	if err != nil {
 		return err
 	}
+	logrus.Debugf("total app impression: %d", len(appImps))
+	wr := entities.NewManager().GetWDbMap()
+
 	for i := range appImps {
 		q := "INSERT INTO daily_report (supplier,type,publisher,imps,cpm,date) VALUES (?,?,?,?,?,?) ON DUPLICATE KEY UPDATE imps=VALUES(imps),cpm=VALUES(cpm)"
-		_, err := entities.NewManager().GetWDbMap().Exec(q, appImps[i].Supplier, "app", appImps[i].Publisher, appImps[i].Imps, appImps[i].CPM, date)
+		_, err := wr.Exec(q, appImps[i].Supplier, "app", appImps[i].Publisher, appImps[i].Imps, appImps[i].CPM, date)
 		if err != nil {
 			return err
 		}
@@ -78,6 +92,10 @@ func AppImp(date int) error {
 
 // WebClick select from click type web
 func WebClick(date int) error {
+	logrus.Debug("start web click worker")
+	defer func() {
+		logrus.Debug("web click worker done")
+	}()
 	var webClicks []clickRes
 	q := fmt.Sprintf(`SELECT COUNT(c_id) AS clicks,
 		COALESCE(SUM(c_winnerbid),0) AS cpc,
@@ -93,18 +111,26 @@ func WebClick(date int) error {
 	if err != nil {
 		return err
 	}
+	logrus.Debugf("total web clicks: %d", len(webClicks))
+	wr := entities.NewManager().GetWDbMap()
+
 	for i := range webClicks {
 		q := "INSERT INTO daily_report (supplier,type,publisher,clicks,cpc,date) VALUES (?,?,?,?,?,?) ON DUPLICATE KEY UPDATE clicks=VALUES(clicks),cpc=VALUES(cpc)"
-		_, err := entities.NewManager().GetWDbMap().Exec(q, webClicks[i].Supplier, "web", webClicks[i].Publisher, webClicks[i].Clicks, webClicks[i].CPC, date)
+		_, err := wr.Exec(q, webClicks[i].Supplier, "web", webClicks[i].Publisher, webClicks[i].Clicks, webClicks[i].CPC, date)
 		if err != nil {
 			return err
 		}
 	}
+
 	return nil
 }
 
 // AppClick select from click type app
 func AppClick(date int) error {
+	logrus.Debug("start app click worker")
+	defer func() {
+		logrus.Debug("app click worker done")
+	}()
 	var appClicks []clickRes
 	q := fmt.Sprintf(`SELECT COUNT(c_id) AS clicks,
 		COALESCE(SUM(c_winnerbid),0) AS cpc,
@@ -120,9 +146,11 @@ func AppClick(date int) error {
 	if err != nil {
 		return err
 	}
+	logrus.Debugf("total app click: %d", len(appClicks))
+	wr := entities.NewManager().GetWDbMap()
 	for i := range appClicks {
 		q := "INSERT INTO daily_report (supplier,type,publisher,clicks,cpc,date) VALUES (?,?,?,?,?,?) ON DUPLICATE KEY UPDATE clicks=VALUES(clicks),cpc=VALUES(cpc)"
-		_, err := entities.NewManager().GetWDbMap().Exec(q, appClicks[i].Supplier, "app", appClicks[i].Publisher, appClicks[i].Clicks, appClicks[i].CPC, date)
+		_, err := wr.Exec(q, appClicks[i].Supplier, "app", appClicks[i].Publisher, appClicks[i].Clicks, appClicks[i].CPC, date)
 		if err != nil {
 			return err
 		}
