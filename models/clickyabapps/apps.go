da02cf56624dffe2e7415c049f16b61ad3029f32
dara <dara51php@gmail.com>
2018-02-17 17:31:52 +0330

    add loader for clickyab app by token
    Fix #4800

---
+++ models/clickyabapps/apps.go
@@ -0,0 +1,28 @@
+package clickyabapps
+
+import (
+	"fmt"
+
+	"clickyab.com/crane/demand/entity"
+	"clickyab.com/crane/models/internal/entities"
+	"github.com/clickyab/services/pool"
+)
+
+var (
+	app pool.Interface
+)
+
+// GetAppOrFake try to get app. do not use it in initializer
+func GetAppOrFake(sup entity.Supplier, appToken string) (entity.Publisher, error) {
+	d := &entities.App{}
+	res, err := app.Get(appToken, d)
+	if err != nil {
+		return nil, err
+	}
+	if d.Status != 1 {
+		return nil, fmt.Errorf("blocked app")
+	}
+	d = res.(*entities.App)
+	d.Supp = sup
+	return d, nil
+}
