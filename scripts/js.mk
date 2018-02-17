d12cd4e3a081e7c39c101054e63ce43a11b43d9a
fzerorubigd <fzerorubigd@gmail.com>
2018-02-17 11:02:05 +0330

    add vast js files

--- scripts/js.mk
+++ scripts/js.mk
@@ -1,8 +1,13 @@
 npm-install:
 	cd $(ROOT)/js/banner && $(NPM) install
+	cd $(ROOT)/js/vmap && $(NPM) install
 
 build-js: npm-install
 	cd $(ROOT)/js/banner && $(NPM) run build
+	rm -rf $(ROOT)/dist
+	cp -r $(ROOT)/js/banner/dist $(ROOT)/js
+	cp $(ROOT)/js/vmap/node_modules/vmap-kit/dist/jwplayer/vmap.js $(ROOT)/js/dist/jwplayer.js
+	cp $(ROOT)/js/vmap/node_modules/vmap-kit/dist/videojs/vmap.js $(ROOT)/js/dist/videojs.js
 
-show-js: build-js go-bindata
-	cd $(ROOT)/js/banner/dist && $(BIN)/go-bindata -nometadata -o $(ROOT)/supplier/layer/web/show-js.gen.go -nomemcopy=true -pkg=web ./
+js: build-js go-bindata
+	cd $(ROOT)/js/dist && $(BIN)/go-bindata -nometadata -o $(ROOT)/supplier/layer/web/show-js.gen.go -nomemcopy=true -pkg=web ./
