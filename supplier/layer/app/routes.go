1666cac6baf1c3f7d7cfb2b12d960d4465ed71df
fzerorubigd <fzerorubigd@gmail.com>
2018-02-18 11:20:35 +0330

    invalid route name

--- supplier/layer/app/routes.go
+++ supplier/layer/app/routes.go
@@ -12,7 +12,7 @@ type controller struct {
 func (controller) Routes(r framework.Mux) {
 	r.GET("app-single-ad", "/api/getapp", getApp)
 	r.GET("app-single-ad-bc", "/ads/inapp.php", getApp)
-	r.GET("app-single-ad-bc", "/ads/json-inapp.php", getInappJSON)
+	r.GET("app-json-app", "/ads/json-inapp.php", getInappJSON)
 }
 
 func init() {
