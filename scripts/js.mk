229e86a5c30a472a17a16f98516f0ce98f9bc9a5
fzerorubigd <fzerorubigd@gmail.com>
2018-02-19 10:53:05 +0330

    update vast js files and library

--- scripts/js.mk
+++ scripts/js.mk
@@ -6,8 +6,8 @@ build-js: npm-install
 	cd $(ROOT)/js/banner && $(NPM) run build
 	rm -rf $(ROOT)/dist
 	cp -r $(ROOT)/js/banner/dist $(ROOT)/js
-	cp $(ROOT)/js/vmap/node_modules/vmap-kit/dist/jwplayer/vmap.js $(ROOT)/js/dist/jwplayer.js
-	cp $(ROOT)/js/vmap/node_modules/vmap-kit/dist/videojs/vmap.js $(ROOT)/js/dist/videojs.js
+	cp $(ROOT)/js/vmap/node_modules/vmap-kit/dist/jwplayer/vast.js $(ROOT)/js/dist/jwplayer.js
+	cp $(ROOT)/js/vmap/node_modules/vmap-kit/dist/videojs/vast.js $(ROOT)/js/dist/videojs.js
 
 js: build-js go-bindata
 	cd $(ROOT)/js/dist && $(BIN)/go-bindata -nometadata -o $(ROOT)/supplier/layer/internal/js/js.gen.go -nomemcopy=true -pkg=js ./
