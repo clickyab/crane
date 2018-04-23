be203dacfb030166ed24af4bc706dc26cdf3d2ff
Ehsan <zerob4wl@gmail.com>
2018-04-23 10:31:03 +0430

    add url suffix

--- js/tracker/src/index.ts
+++ js/tracker/src/index.ts
@@ -23,11 +23,11 @@
   function getQueryStrings() {
     let assoc: {
       [key: string]: string
-    } = {}
-    const queryString = location.search.substring(1)
-    const keyValues = queryString.split('&')
+    } = {};
+    const queryString = location.search.substring(1);
+    const keyValues = queryString.split('&');
     for (let i in keyValues) {
-      let key = keyValues[i].split('=')
+      let key = keyValues[i].split('=');
       if (key.length > 1) {
         assoc[decode(key[0])] = decode(key[1])
       }
@@ -36,8 +36,8 @@
   }
 
   function appendImgHit(click: string | null, imp: string, actionId: string) {
-    const imgHit = document.createElement('img')
-    imgHit.setAttribute('src', '{{.URL}}')
+    const imgHit = document.createElement('img');
+    imgHit.setAttribute('src', `{{.URL}}?click_id=${click}imp_id=${imp}&action_id=${actionId}`);
     document.body.appendChild(imgHit)
   }
 
@@ -51,16 +51,16 @@
     }
   }
 
-  let getWholeQuery = getQueryStrings()
-  cyClick = getWholeQuery['cy_click']
-  cyImp = getWholeQuery['cy_imp']
+  let getWholeQuery = getQueryStrings();
+  cyClick = getWholeQuery['cy_click'];
+  cyImp = getWholeQuery['cy_imp'];
 
   if (cyClick && cyImp) {
     document.cookie = 'cy_click=' + cyClick + '; expires=Fri, 31 Dec 2022 23:59:59 GMT'
     document.cookie = 'cy_imp=' + cyImp + '; expires=Fri, 31 Dec 2022 23:59:59 GMT'
   } else {
-    cyClick = getCookie('cy_click')
-    cyClick = getCookie('cy_imp')
+    cyClick = getCookie('cy_click');
+    cyClick = getCookie('cy_imp');
   }
 
   ;(window as any).clickyab_callback = clickyab_callback
