b6109730ad54e7e2bdb88ff4d41613d43298024d
slifer2015 <dara51php@gmail.com>
2018-05-05 19:12:21 +0430

    native templates implementation
    Fix #5184

--- supplier/layers/native/native.go
+++ supplier/layers/native/native.go
@@ -4,14 +4,18 @@ import (
 	"context"
 	"net/http"
 	"strconv"
+	"sync"
 
 	"fmt"
 
-	website "clickyab.com/crane/models/website"
+	"errors"
+
+	"clickyab.com/crane/models/website"
 	"clickyab.com/crane/supplier/client"
 	"clickyab.com/crane/supplier/layers/internal/supplier"
 	"clickyab.com/crane/supplier/layers/output"
 	"github.com/bsm/openrtb"
+	"github.com/clickyab/services/array"
 	"github.com/clickyab/services/assert"
 	"github.com/clickyab/services/config"
 	"github.com/clickyab/services/framework"
@@ -22,11 +26,19 @@ import (
 )
 
 var (
-	nativeMaxCount    = config.RegisterInt("crane.supplier.native.max_count", 12, "")
-	nativeMaxTitleLen = config.RegisterInt("crane.supplier.native.title,len", 50, "")
-	server            = config.RegisterString("crane.supplier.banner.url", "", "route for banner")
-	method            = config.RegisterString("crane.supplier.banner.method", "POST", "method for banner request")
+	nativeMaxCount        = config.RegisterInt("crane.supplier.native.max_count", 12, "")
+	nativeMaxTitleLen     = config.RegisterInt("crane.supplier.native.title,len", 50, "")
+	defaultNativeTemplate = config.RegisterString("crane.supplier.native.default.template", "grid4x", "")
+	server                = config.RegisterString("crane.supplier.banner.url", "http://127.0.0.1:8090/api/ortb/forbidden", "route for banner")
+	method                = config.RegisterString("crane.supplier.banner.method", "POST", "method for banner request")
+	template              = make(map[string]temp)
+	lock                  = sync.RWMutex{}
 )
+
+type temp struct {
+	validCounts []int
+	image, text bool
+}
 
 // ImageType openrtb native image
 type ImageType int
@@ -46,9 +58,37 @@ var (
 // ref			:referrer
 // parent		:parent
 // count		:number of impression
+// t			:template (grid3x,grid4x,single,text-list)
 // handle supplier native route
 func getNative(ctx context.Context, w http.ResponseWriter, r *http.Request) {
 	domain := r.URL.Query().Get("d")
+	temp := r.URL.Query().Get("t")
+	count := r.URL.Query().Get("count")
+	intCount, err := strconv.Atoi(count)
+	if intCount > nativeMaxCount.Int() {
+		intCount = nativeMaxCount.Int()
+	}
+	if err != nil || intCount < 1 {
+		xlog.GetWithError(ctx, err).Debug("wrong count")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	if temp == "" {
+		temp = defaultNativeTemplate.String()
+	}
+	lock.RLock()
+	defer lock.RUnlock()
+	s, ok := template[temp]
+	if !ok {
+		xlog.GetWithError(ctx, errors.New("native template is invalid")).Debug("native template is invalid")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	if !array.IntInArray(intCount, s.validCounts...) {
+		xlog.GetWithError(ctx, errors.New("wrong native count for template")).Debug("wrong native count for template")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
 	pub, err := website.GetWebSite(sup, domain, 0)
 	if err != nil {
 		xlog.GetWithError(ctx, err).Debug("no website")
@@ -63,18 +103,6 @@ func getNative(ctx context.Context, w http.ResponseWriter, r *http.Request) {
 	ip := framework.RealIP(r)
 	useragent := r.UserAgent()
 
-	count := r.URL.Query().Get("count")
-	intCount, err := strconv.Atoi(count)
-	if err != nil || intCount < 1 {
-		xlog.GetWithError(ctx, err).Debug("wrong count")
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	if intCount > nativeMaxCount.Int() {
-		intCount = nativeMaxCount.Int()
-	}
-
 	ua := user_agent.New(useragent)
 
 	mi := 0
@@ -87,7 +115,7 @@ func getNative(ctx context.Context, w http.ResponseWriter, r *http.Request) {
 		User: &openrtb.User{
 			ID: nativeUserIDGenerator(tid, useragent, ip),
 		},
-		Imp: getImps(r, intCount, pub),
+		Imp: getImps(r, intCount, pub, s.image, s.text),
 		Site: &openrtb.Site{
 			Page:   parent,
 			Ref:    ref,
@@ -119,13 +147,51 @@ func getNative(ctx context.Context, w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	result, err := output.RenderNative(ctx, br)
+	//validate len of response
+	br.SeatBid = reduceNativeSeat(br, intCount, s.validCounts)
+	result, err := output.RenderNative(ctx, br, !s.image)
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
 	_, err = w.Write(result)
 	assert.Nil(err)
+}
+
+// reduceNativeSeat reduce based on their template configs
+func reduceNativeSeat(br *openrtb.BidResponse, intCount int, validCounts []int) []openrtb.SeatBid {
+	bidRespCount := len(br.SeatBid)
+	if bidRespCount < intCount {
+		var valid int
+		for i := range validCounts {
+			if bidRespCount >= validCounts[i] {
+				valid = validCounts[i]
+			}
+		}
+		return br.SeatBid[0:valid]
+	}
+	return br.SeatBid
+}
+
+// registerTemp
+func registerTemplate(name string, count []int, image, text bool) {
+	lock.Lock()
+	defer lock.Unlock()
+	_, ok := template[name]
+	assert.False(ok)
+	template[name] = temp{
+		validCounts: count,
+		image:       image,
+		text:        text,
+	}
+}
+
+// init register custom clickyab native template
+func init() {
+	registerTemplate("grid3x", []int{3, 6, 12}, true, true)
+	registerTemplate("grid4x", []int{4, 8, 12}, true, true)
+	registerTemplate("single", []int{1}, true, true)
+	registerTemplate("text-list", []int{1, 3, 4, 6, 8, 12}, false, true)
 }
 
 // nativeUserIDGenerator create user id for native
