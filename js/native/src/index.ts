7914b31face202efc7e03b18236ee5946fa5d675
Ehsan <zerob4wl@gmail.com>
2018-05-05 17:37:06 +0430

    New templates

--- js/native/src/index.ts
+++ js/native/src/index.ts
@@ -5,7 +5,8 @@ const elements = document.getElementsByClassName("clickyab-native");
 
 for (let i = 0; i < elements.length; i++) {
 	const wrapper: HTMLElement = elements.item(i) as HTMLElement;
-	const url = "{{.URL}}?tid=__tid__&i=__id__&d=__domain__&count=__count__";
+	// const url = "//supplier.clickyab.com/api/get/native?tid=__tid__&i=__id__&d=__domain__&count=__count__&t=__type__";
+	const url = "{{.URL}}?tid=__tid__&i=__id__&d=__domain__&count=__count__&t=__type__";
 
 	const nativeComponent = new NativeComponent(wrapper, url);
 }
