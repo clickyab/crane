8d375193ff00a2c0baf7e9945b4aa765afacad7f
slifer2015 <dara51php@gmail.com>
2018-05-05 19:12:53 +0430

    update vendor

--- vendor/github.com/clickyab/services/array/string.go
+++ vendor/github.com/clickyab/services/array/string.go
@@ -10,3 +10,14 @@ func StringInArray(in string, arr ...string) bool {
 
 	return false
 }
+
+// IntInArray return true if array contain the number
+func IntInArray(in int, arr ...int) bool {
+	for i := range arr {
+		if arr[i] == in {
+			return true
+		}
+	}
+
+	return false
+}
