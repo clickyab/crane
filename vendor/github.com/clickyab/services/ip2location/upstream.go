e06c5fefe0dd1bcdd4ddc0c34e53de9f2de7ab8d
Mehrdad Dadkhah <mehrdad@dadkhah.me>
2018-05-13 10:10:25 +0430

    better log in ip2location service

--- vendor/github.com/clickyab/services/ip2location/upstream.go
+++ vendor/github.com/clickyab/services/ip2location/upstream.go
@@ -7,6 +7,8 @@ import (
 	"math/big"
 	"net"
 	"strconv"
+
+	"github.com/sirupsen/logrus"
 )
 
 // metaData
@@ -196,7 +198,7 @@ func readUint8(pos int64) uint8 {
 	data := make([]byte, 1)
 	_, err := f.ReadAt(data, pos-1)
 	if err != nil {
-		fmt.Println("File read failed:", err)
+		logrus.Debugln("File read failed in readUint8 func:", err, " file is: ", f.file.Name())
 	}
 	retval = data[0]
 	return retval
@@ -209,12 +211,12 @@ func readUint32(pos uint32) uint32 {
 	data := make([]byte, 4)
 	_, err := f.ReadAt(data, pos2-1)
 	if err != nil {
-		fmt.Println("File read failed:", err)
+		logrus.Debugln("File read failed in readUint32 func:", err, " file is: ", f.file.Name())
 	}
 	buf := bytes.NewReader(data)
 	err = binary.Read(buf, binary.LittleEndian, &retval)
 	if err != nil {
-		fmt.Println("Binary read failed:", err)
+		logrus.Debugln("Binary read failed in readUint32 func:", err, " file is: ", f.file.Name())
 	}
 	return retval
 }
@@ -226,7 +228,7 @@ func readUint128(pos uint32) *big.Int {
 	data := make([]byte, 16)
 	_, err := f.ReadAt(data, pos2-1)
 	if err != nil {
-		fmt.Println("File read failed:", err)
+		logrus.Debugln("File read failed in readUint128 func:", err, " file is: ", f.file.Name())
 	}
 
 	// little endian to big endian
@@ -244,13 +246,13 @@ func readStr(pos uint32) string {
 	lenbyte := make([]byte, 1)
 	_, err := f.ReadAt(lenbyte, pos2)
 	if err != nil {
-		fmt.Println("File read failed:", err)
+		logrus.Debugln("File read failed in readStr func:", err, " file is: ", f.file.Name())
 	}
 	strlen := lenbyte[0]
 	data := make([]byte, strlen)
 	_, err = f.ReadAt(data, pos2+1)
 	if err != nil {
-		fmt.Println("File read failed:", err)
+		logrus.Debugln("File read failed in readStr func:", err, " file is: ", f.file.Name())
 	}
 	retval = string(data[:strlen])
 	return retval
@@ -263,12 +265,12 @@ func readFloat(pos uint32) float32 {
 	data := make([]byte, 4)
 	_, err := f.ReadAt(data, pos2-1)
 	if err != nil {
-		fmt.Println("File read failed:", err)
+		logrus.Debugln("File read failed in readFloat func:", err, " file is: ", f.file.Name())
 	}
 	buf := bytes.NewReader(data)
 	err = binary.Read(buf, binary.LittleEndian, &retval)
 	if err != nil {
-		fmt.Println("Binary read failed:", err)
+		logrus.Debugln("File read failed in readFloat func:", err, " file is: ", f.file.Name())
 	}
 	return retval
 }
