4d10840ec094d45eff1cc588302eb9e48f0caaed
slifer2015 <dara51php@gmail.com>
2018-05-23 11:05:18 +0430

    fix implement clickyab supplier

--- supplier/layers/internal/supplier/supplier.go
+++ supplier/layers/internal/supplier/supplier.go
@@ -8,19 +8,19 @@ type dummy struct {
 }
 
 func (s *dummy) SoftFloorCPC(string, string) int64 {
-	panic("implement me")
+	panic("implement dummy SoftFloorCPC not valid")
 }
 
 func (s *dummy) SoftFloorCPM(string, string) int64 {
-	panic("implement me")
+	panic("implement dummy SoftFloorCPM not valid")
 }
 
 func (s *dummy) Share() int {
-	panic("implement me")
+	panic("implement dummy Share not valid")
 }
 
 func (s *dummy) UnderFloor() bool {
-	panic("implement me")
+	panic("implement dummy UnderFloor not valid")
 }
 
 func (s *dummy) Name() string {
@@ -28,21 +28,21 @@ func (s *dummy) Name() string {
 }
 
 func (s *dummy) Token() string {
-	panic("implement me")
+	panic("implement dummy Token not valid")
 }
 
 func (s *dummy) DefaultSoftFloorCPM() int64 {
-	panic("implement me")
+	panic("implement dummy DefaultSoftFloorCPM not valid")
 }
 
 func (s *dummy) DefaultMinBid() int64 {
-	panic("implement me")
+	panic("implement dummy DefaultMinBid not valid")
 }
 func (s *dummy) Strategy() entity.Strategy {
-	panic("implement me")
+	panic("implement dummy Strategy not valid")
 }
 func (s *dummy) DefaultCTR(string, string) float64 {
-	panic("implement me")
+	panic("implement dummy DefaultCTR not valid")
 }
 
 func (s *dummy) AllowCreate() bool {
@@ -50,27 +50,27 @@ func (s *dummy) AllowCreate() bool {
 }
 
 func (s *dummy) TinyMark() bool {
-	panic("implement me")
+	panic("implement dummy TinyMark not valid")
 }
 
 func (s *dummy) TinyLogo() string {
-	panic("implement me")
+	panic("implement dummy TinyLogo not valid")
 }
 
 func (s *dummy) TinyURL() string {
-	panic("implement me")
+	panic("implement dummy TinyURL not valid")
 }
 
 func (s *dummy) ShowDomain() string {
-	panic("implement me")
+	panic("implement dummy ShowDomain not valid")
 }
 
 func (s *dummy) UserID() int64 {
-	panic("implement me")
+	panic("implement dummy UserID not valid")
 }
 
 func (s *dummy) Rate() int {
-	panic("implement me")
+	panic("implement dummy Rate not valid")
 }
 
 // NewClickyab return a dummy clickyab supplier
