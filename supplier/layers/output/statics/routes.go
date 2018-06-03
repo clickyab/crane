244223b1ae6bd3fde1109f2e33de837c05e1161a
Mehrdad Dadkhah <mehrdad@dadkhah.me>
2018-06-03 13:43:09 +0430

    add robots.txt to demand and supplier and noindex meta tags

--- supplier/layers/output/statics/routes.go
+++ supplier/layers/output/statics/routes.go
@@ -3,12 +3,16 @@ package statics
 import (
 	"context"
 	"net/http"
-	"os"
 	"path/filepath"
 
+	"clickyab.com/services/config"
 	"github.com/clickyab/services/framework"
 	"github.com/clickyab/services/framework/router"
 	"github.com/rs/xhandler"
+)
+
+var (
+	staticsPath = config.RegisterString("crane.statics.path", "/app/statics", "determine statics full path")
 )
 
 type controller struct {
@@ -16,13 +20,13 @@ type controller struct {
 
 // router handler to serve static files
 func (c controller) serveStatic(ctx context.Context, w http.ResponseWriter, r *http.Request) {
-	fullPath, _ := filepath.Abs(os.Getenv("ROOT") + "/statics/" + r.URL.Path[len("/static/"):])
+	fullPath, _ := filepath.Abs(staticsPath.String() + "/statics/" + r.URL.Path[len("/static/"):])
 
 	http.ServeFile(w, r, fullPath)
 }
 
 func (c controller) serveRobot(ctx context.Context, w http.ResponseWriter, r *http.Request) {
-	fullPath, _ := filepath.Abs(os.Getenv("ROOT") + "/statics/robots.txt")
+	fullPath, _ := filepath.Abs(staticsPath.String() + "/statics/robots.txt")
 
 	http.ServeFile(w, r, fullPath)
 }
