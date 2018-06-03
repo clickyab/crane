f5417c7b64cf3d384a66331223e1baf3abba7361
Mehrdad Dadkhah <mehrdad@dadkhah.me>
2018-06-03 13:43:09 +0430

    add robots.txt to demand and supplier and noindex meta tags

--- demand/layers/output/banner/banner.go
+++ demand/layers/output/banner/banner.go
@@ -14,6 +14,7 @@ const bannerTemplateText = `<!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta name="robots" content="nofollow">
+	<meta name="robots" content="noindex">
 	<meta content="text/html; charset=utf-8" http-equiv="Content-Type">
 	<style>
 	html, body, div, span, applet, object, iframe,h1, h2, h3, h4, h5, h6, p, blockquote, pre,a, abbr, acronym, address, big, cite, code,del, dfn, em, font, img, ins, kbd, q, s, samp,small, strike, strong, sub, sup, tt, var,dl, dt, dd, ol, ul, li,fieldset, form, label, legend,table, caption, tbody, tfoot, thead, tr, th, td { margin: 0; padding: 0; border: 0; outline: 0; font-weight: inherit; font-style: inherit; font-size: 100%; font-family: inherit; vertical-align: baseline;}
