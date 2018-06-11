84ea8620dd2070799fa7fef94f53744ef1d19e3a
slifer2015 <dara51php@gmail.com>
2018-06-11 13:21:58 +0430

    fix heroku

--- scripts/herokutor.sh
+++ scripts/herokutor.sh
@@ -62,7 +62,7 @@ popd
 
 [ -z ${APP} ] && exit_message "The APP is not defined." # WTF, the APP NAME is important
 [ -z ${CHANGE_AUTHOR} ] || exit_message "It's a PR, bail out" 0
-if [[ ( "${BRANCH}" != "master" ) && ( "${BRANCH}" != "dev" ) ]]; then
+if [[ ( "${BRANCH}" != "master" ) && ( "${BRANCH}" != "dev" ) && ( "${BRANCH}" != "revert" ) ]]; then
     exit_message "Its not on correct branch, bail out" 0
 fi
 
@@ -109,7 +109,7 @@ TARGET=$(mktemp -d)
 pushd ${TEMPORARY}
 # Actual build
 PUSH="--push"
-if [[ ( "${BRANCH}" != "master" ) && ( "${BRANCH}" != "dev" ) ]]; then
+if [[ ( "${BRANCH}" != "master" ) && ( "${BRANCH}" != "dev" ) && ( "${BRANCH}" != "revert" ) ]]; then
     PUSH=""
 fi
 rocker build --no-cache ${PUSH} -var Build=${BUILD} -var EnvDir=${VARS} -var Cache=${CACHE} -var Target=${TARGET} -var Version=${COMMIT_COUNT} -var App=${APP}_${BRANCH}
@@ -119,6 +119,12 @@ NAMESPACE="${APP}"
 VERSION="${COMMIT_COUNT}"
 if [[ "${BRANCH}" == "dev" ]]; then
     NAMESPACE=${APP}-staging
+    #VERSION="latest"
+fi
+
+if [[ "${BRANCH}" == "revert" ]]; then
+    NAMESPACE=${APP}
+    BRANCH="master"
     #VERSION="latest"
 fi
 
