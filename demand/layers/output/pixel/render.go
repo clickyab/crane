6b35aa1e785af0a1d89f3deba4daaf3a517f6ffc
slifer2015 <dara51php@gmail.com>
2018-07-15 17:50:29 +0430

    custom coversion

--- demand/layers/output/pixel/render.go
+++ demand/layers/output/pixel/render.go
@@ -12,9 +12,13 @@ import (
 const emptyPixel = "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABAQMAAAAl21bKAAAAA1BMVEUAAACnej3aAAAAAXRSTlMAQObYZgAAAApJREFUCNdjYAAAAAIAAeIhvDMAAAAASUVORK5CYII="
 
 // Render an empty pixel
-func Render(_ context.Context, w http.ResponseWriter, _ entity.Context) error {
+func Render(_ context.Context, w http.ResponseWriter, _ entity.Context, stat bool) error {
 	data, err := base64.StdEncoding.DecodeString(emptyPixel)
 	assert.Nil(err)
+	if !stat {
+		w.WriteHeader(http.StatusBadRequest)
+		return nil
+	}
 	w.Header().Set("Content-Type", "image/png")
 	_, _ = w.Write(data)
 	return nil
