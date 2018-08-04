1746faf17da94962d520f3d1d8a8069038203d9e
Kian Ostad <c@kianostad.com>
2018-08-05 01:46:16 +0430

    fix worker

--- vendor/github.com/clickyab/services/broker/rabbitmq/initializer.go
+++ vendor/github.com/clickyab/services/broker/rabbitmq/initializer.go
@@ -155,7 +155,6 @@ func (in *initRabbit) Healthy(context.Context) error {
 
 // Initialize the module at the beginning of the application to create a publish channel
 func (in *initRabbit) Initialize(ctx context.Context) {
-
 	once.Do(func() {
 		// the size is here for channel to not block the caller. since we read this on the health check command
 		in.notifyCloser = make(chan *amqp.Error, 10)
@@ -210,7 +209,7 @@ func (in *initRabbit) Initialize(ctx context.Context) {
 			pchn.NotifyPublish(rtrn)
 			tmp := chnlLock{
 				chn:    pchn,
-				lock:   &sync.Mutex{},
+				Mutex:   &sync.Mutex{},
 				wg:     &sync.WaitGroup{},
 				rtrn:   rtrn,
 				closed: false,
