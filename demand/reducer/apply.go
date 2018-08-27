03aed089899b04575241d50550303c04aa29ae16
Kian Ostad <c@kianostad.com>
2018-08-27 16:37:19 +0430

    fix filter

--- demand/reducer/apply.go
+++ demand/reducer/apply.go
@@ -3,8 +3,6 @@ package reducer
 import (
 	"context"
 
-	"sync"
-
 	"fmt"
 
 	"time"
@@ -22,65 +20,36 @@ type Filter interface {
 // result is the accepted items
 func Apply(_ context.Context, imp entity.Context, ads []entity.Creative, ff []Filter) []entity.Creative {
 	var mads = make([]map[int64]entity.Creative, 0)
-
 	var m = make([]entity.Creative, 0, len(ads))
-	lock := sync.RWMutex{}
-	var done = make(chan int)
-	var cancel = make(chan error)
 
 	for _, f := range ff {
-		go func(f Filter) {
-			var fads = make(map[int64]entity.Creative)
-			var err error
-			for i := range ads {
-				if ferr := f.Check(imp, ads[i]); ferr != nil {
-					err = ferr
-					if i == len(ads)-1 {
-						cancel <- err
-					}
-					continue
-				}
-				fads[ads[i].ID()] = ads[i]
+		var fads = make(map[int64]entity.Creative)
+		var err error
+		for i := range ads {
+			if ferr := f.Check(imp, ads[i]); ferr != nil {
+				err = ferr
 			}
-			if len(fads) == 0 && len(ads) != 0 {
-				cancel <- err
-				return
+		}
+		if len(fads) == 0 && len(ads) != 0 {
+			if imp.Publisher().Supplier().Name() != "clickyab" {
+				iqs := kv.NewAEAVStore(fmt.Sprintf("DEQS_%s", time.Now().Truncate(time.Hour*24).Format("060102")), time.Hour*72)
+				iqs.IncSubKey(fmt.Sprintf("%s_%s_%s", imp.Publisher().Supplier().Name(), time.Now().Truncate(time.Hour).Format("15"), err.Error()), 1)
 			}
-			lock.Lock()
-			mads = append(mads, fads)
-			lock.Unlock()
-			done <- 0
-		}(f)
-	}
-
-	var c int
-SELECT:
-	for {
-		select {
-		case <-time.After(time.Millisecond * 100):
-			break SELECT
-		case <-done:
-			c++
-			if len(ff) == c {
-				ref := mads[0]
-			LOOP:
-				for kr, vr := range ref {
-					for _, v := range mads[1:] {
-						if _, ok := v[kr]; !ok {
-							continue LOOP
-						}
-					}
-
-					m = append(m, vr)
-				}
-				break SELECT
-			}
-
-		case err := <-cancel:
-			iqs := kv.NewAEAVStore(fmt.Sprintf("DEQS_%s", time.Now().Truncate(time.Hour*24).Format("060102")), time.Hour*72)
-			iqs.IncSubKey(fmt.Sprintf("%s_%s_%s", imp.Publisher().Supplier().Name(), time.Now().Truncate(time.Hour).Format("15"), err.Error()), 1)
 			return nil
 		}
+		mads = append(mads, fads)
+	}
+
+	ref := mads[0]
+LOOP:
+	for kr, vr := range ref {
+		for _, v := range mads[1:] {
+			if _, ok := v[kr]; !ok {
+				continue LOOP
+			}
+		}
+
+		m = append(m, vr)
 	}
 
 	return m
