4fb9f383edcd6e4a55fa0ec31bdfaf10801089e4
Kian Ostad <c@kianostad.com>
2018-09-02 22:50:56 +0430

    diable monitoring

--- demand/layers/input/ortb/ortb.go
+++ demand/layers/input/ortb/ortb.go
@@ -10,8 +10,6 @@ import (
 
 	"time"
 
-	"strconv"
-
 	"clickyab.com/crane/demand/builder"
 	"clickyab.com/crane/demand/entity"
 	"clickyab.com/crane/demand/filter"
@@ -25,7 +23,6 @@ import (
 	"github.com/bsm/openrtb/native/request"
 	"github.com/clickyab/services/assert"
 	"github.com/clickyab/services/config"
-	"github.com/clickyab/services/kv"
 	"github.com/clickyab/services/xlog"
 	"github.com/rs/xmux"
 )
@@ -33,8 +30,8 @@ import (
 const demandPath = "/ortb/:token"
 
 var (
-	monitoringSuppliers = config.RegisterString("crane.rtb.monitor.suppliers", "clickyab,chavoosh", "comma separated suppliers name ")
-	deadline            = config.RegisterDuration("crane.rtb.deadline", time.Millisecond*250, "maximum waiting time for ad")
+	//monitoringSuppliers = config.RegisterString("crane.rtb.monitor.suppliers", "clickyab,chavoosh", "comma separated suppliers name ")
+	deadline = config.RegisterDuration("crane.rtb.deadline", time.Millisecond*250, "maximum waiting time for ad")
 )
 var (
 	ortbWebSelector = []reducer.Filter{
@@ -67,90 +64,92 @@ func writesErrorStatus(w http.ResponseWriter, status int, detail string) {
 	w.WriteHeader(status)
 	_, _ = fmt.Fprint(w, detail)
 }
-func monitoring(tk time.Time, sup string) {
 
-	msup := strings.Split(monitoringSuppliers.String(), ",")
-	if len(msup) == 0 {
-		return
-	}
-
-	window := time.Second * 5
-	ckey := time.Now().Truncate(window).Format("TRMS_060102150405")
-	okey := time.Now().Truncate(window).Add(window * -1).Format("TRMS_060102150405")
-
-	tm := time.Since(tk).Nanoseconds() / 1e6
-
-	rms := kv.NewAEAVStore(ckey, window*10)
-	max := rms.AllKeys()[fmt.Sprintf("%s_X", sup)]
-	min := rms.AllKeys()[fmt.Sprintf("%s_M", sup)]
-	rms.IncSubKey(fmt.Sprintf("%s_T", sup), tm)
-	rms.IncSubKey(fmt.Sprintf("%s_C", sup), 1)
-
-	if cat := tm / 10; cat > 9 {
-		rms.IncSubKey(fmt.Sprintf("%s_10", sup), 1)
-	} else {
-		rms.IncSubKey(fmt.Sprintf("%s_%d", sup, cat), 1)
-	}
-
-	tms := kv.NewEavStore(ckey)
-	update := false
-	if tm > max {
-		tms.SetSubKey(fmt.Sprintf("%s_X", sup), fmt.Sprintf("%d", tm+1))
-		update = true
-	}
-	if min == 0 || tm < min {
-		tms.SetSubKey(fmt.Sprintf("%s_M", sup), fmt.Sprintf("%d", tm+1))
-		update = true
-	}
-	if update {
-		assert.Nil(tms.Save(window * 10))
-	}
-	old := kv.NewEavStore(okey)
-
-	for _, ms := range msup {
-		current := kv.NewEavStore(fmt.Sprintf("RMQS_%s", ms))
-		if current.AllKeys()["DATE"] == okey {
-			return
-		}
-		current.SetSubKey("DATE", okey)
-		current.SetSubKey("MAX", old.AllKeys()[fmt.Sprintf("%s_X", ms)])
-		t, _ := strconv.ParseInt(old.AllKeys()[fmt.Sprintf("%s_T", ms)], 10, 64)
-		c, _ := strconv.ParseInt(old.AllKeys()[fmt.Sprintf("%s_C", ms)], 10, 64)
-
-		if t != 0 && c != 0 {
-			current.SetSubKey("AVG", fmt.Sprintf("%d ms", t/c))
-			current.SetSubKey("COUNT", fmt.Sprintf("%d p/s", c/5))
-
-		}
-		for i := 0; i < 11; i++ {
-			ps, _ := strconv.ParseInt(old.AllKeys()[fmt.Sprintf("%s_%d", ms, i)], 10, 64)
-			if ps > 0 && c > 0 {
-				if i == 10 {
-					current.SetSubKey(fmt.Sprintf("%03d0ms>", i), fmt.Sprintf("%-3d%%  %d", (ps*100)/c, ps))
-					continue
-				}
-				current.SetSubKey(fmt.Sprintf(">%03d0ms>", i), fmt.Sprintf("%-3d%%  %d", (ps*100)/c, ps))
-				continue
-			}
-
-			current.SetSubKey(fmt.Sprintf(">%03d0ms>", i), fmt.Sprintf("%-3d%%  %d", 0, 0))
-
-		}
-		assert.Nil(current.Save(window * 100))
-
-	}
-
-}
+//
+//func monitoring(tk time.Time, sup string) {
+//
+//	msup := strings.Split(monitoringSuppliers.String(), ",")
+//	if len(msup) == 0 {
+//		return
+//	}
+//
+//	window := time.Second * 5
+//	ckey := time.Now().Truncate(window).Format("TRMS_060102150405")
+//	okey := time.Now().Truncate(window).Add(window * -1).Format("TRMS_060102150405")
+//
+//	tm := time.Since(tk).Nanoseconds() / 1e6
+//
+//	rms := kv.NewAEAVStore(ckey, window*10)
+//	max := rms.AllKeys()[fmt.Sprintf("%s_X", sup)]
+//	min := rms.AllKeys()[fmt.Sprintf("%s_M", sup)]
+//	rms.IncSubKey(fmt.Sprintf("%s_T", sup), tm)
+//	rms.IncSubKey(fmt.Sprintf("%s_C", sup), 1)
+//
+//	if cat := tm / 10; cat > 9 {
+//		rms.IncSubKey(fmt.Sprintf("%s_10", sup), 1)
+//	} else {
+//		rms.IncSubKey(fmt.Sprintf("%s_%d", sup, cat), 1)
+//	}
+//
+//	tms := kv.NewEavStore(ckey)
+//	update := false
+//	if tm > max {
+//		tms.SetSubKey(fmt.Sprintf("%s_X", sup), fmt.Sprintf("%d", tm+1))
+//		update = true
+//	}
+//	if min == 0 || tm < min {
+//		tms.SetSubKey(fmt.Sprintf("%s_M", sup), fmt.Sprintf("%d", tm+1))
+//		update = true
+//	}
+//	if update {
+//		assert.Nil(tms.Save(window * 10))
+//	}
+//	old := kv.NewEavStore(okey)
+//
+//	for _, ms := range msup {
+//		current := kv.NewEavStore(fmt.Sprintf("RMQS_%s", ms))
+//		if current.AllKeys()["DATE"] == okey {
+//			return
+//		}
+//		current.SetSubKey("DATE", okey)
+//		current.SetSubKey("MAX", old.AllKeys()[fmt.Sprintf("%s_X", ms)])
+//		t, _ := strconv.ParseInt(old.AllKeys()[fmt.Sprintf("%s_T", ms)], 10, 64)
+//		c, _ := strconv.ParseInt(old.AllKeys()[fmt.Sprintf("%s_C", ms)], 10, 64)
+//
+//		if t != 0 && c != 0 {
+//			current.SetSubKey("AVG", fmt.Sprintf("%d ms", t/c))
+//			current.SetSubKey("COUNT", fmt.Sprintf("%d p/s", c/5))
+//
+//		}
+//		for i := 0; i < 11; i++ {
+//			ps, _ := strconv.ParseInt(old.AllKeys()[fmt.Sprintf("%s_%d", ms, i)], 10, 64)
+//			if ps > 0 && c > 0 {
+//				if i == 10 {
+//					current.SetSubKey(fmt.Sprintf("%03d0ms>", i), fmt.Sprintf("%-3d%%  %d", (ps*100)/c, ps))
+//					continue
+//				}
+//				current.SetSubKey(fmt.Sprintf(">%03d0ms>", i), fmt.Sprintf("%-3d%%  %d", (ps*100)/c, ps))
+//				continue
+//			}
+//
+//			current.SetSubKey(fmt.Sprintf(">%03d0ms>", i), fmt.Sprintf("%-3d%%  %d", 0, 0))
+//
+//		}
+//		assert.Nil(current.Save(window * 100))
+//
+//	}
+//
+//}
 
 // openRTBInput is the route for rtb input layer
 func openRTBInput(ct context.Context, w http.ResponseWriter, r *http.Request) {
 	ctx, _ := context.WithTimeout(ct, deadline.Duration())
 
-	tk := time.Now()
+	//tk := time.Now()
 
 	token := xmux.Param(ctx, "token")
 	sup, err := suppliers.GetSupplierByToken(token)
-	defer monitoring(tk, sup.Name())
+	//defer monitoring(tk, sup.Name())
 
 	if err != nil {
 		e := fmt.Sprintf("supplier with token %s not found", token)
