84c9d3af805ac84e4d99abfd2c801bd898538505
Kian Ostad <c@kianostad.com>
2018-09-25 14:07:51 +0330

    vendor update

--- vendor/github.com/bsm/openrtb/native/request/title.go
+++
@@ -1,8 +0,0 @@
-package request
-
-import "github.com/bsm/openrtb"
-
-type Title struct {
-	Length int               `json:"len"` // Maximum length of the text in the title element
-	Ext    openrtb.Extension `json:"ext,omitempty"`
-}
