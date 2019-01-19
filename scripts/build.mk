38c1ac9134cd8187277b2582c1d757dcd6937257
Kian Ostad <c@kianostad.com>
2019-01-19 16:16:26 +0330

    update vendor

--- scripts/build.mk
+++ scripts/build.mk
@@ -1,2 +1,8 @@
 godep:
 	$(GO) install -v github.com/tools/godep
+
+grpc:
+	$(GO) get -u -v google.golang.org/grpc
+	$(GO) get -u -v github.com/golang/protobuf/protoc-gen-go
+	protoc -I $(ROOT)/openrtb $(ROOT)/openrtb/*.proto --go_out=plugins=grpc:$(GOPATH)/src
+	sed -i -r 's/"IAB([[:digit:]]+)S([[:digit:]]+)"/"IAB\1-\2"/' $(ROOT)/openrtb/v2.5/ortb.pb.go
