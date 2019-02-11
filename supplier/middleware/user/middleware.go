7661bd6db909a7c86a1efaad6c43ae36a071b7e2
Kian Ostad <c@kianostad.com>
2019-02-11 23:26:45 +0330

    jumper

--- supplier/middleware/user/middleware.go
+++ supplier/middleware/user/middleware.go
@@ -45,7 +45,6 @@ func (middleware) Handler(next framework.Handler) framework.Handler {
 		}
 
 		fmt.Println("DOMAIN:", r.URL.Hostname())
-
 		user := &openrtb.User{
 			Data: make([]*openrtb.UserData, 0),
 		}
