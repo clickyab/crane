c52d557c5c9318b6c84d4272d1baf20f89da55a3
Kian Ostad <c@kianostad.com>
2019-02-13 15:31:12 +0330

    fix asset handler

--- supplier/layers/asset/asset.go
+++ supplier/layers/asset/asset.go
@@ -32,40 +32,27 @@ func (controller) Routes(r framework.Mux) {
 
 // https://t.clickyab.com/pixel/collecting?list=bamilo&url=https%3A%2F%2Fwww.bamilo.com%2Fproduct%2Factive-%25D9%25BE%25D9%2588%25D8%25AF%25D8%25B1-%25D9%2584%25D8%25A8%25D8%25A7%25D8%25B3%25D8%25B4%25D9%2588%25DB%258C%25DB%258C-%25D9%2585%25D8%25A7%25D8%25B4%25DB%258C%25D9%2586%25DB%258C-500-%25DA%25AF%25D8%25B1%25D9%2585%25DB%258C-9395631%2F&img=%2F%2Fmedia.bamilo.com%2Fp%2Factive-1843-1365939-1-zoom.jpg&title=%D9%BE%D9%88%D8%AF%D8%B1%20%D9%84%D8%A8%D8%A7%D8%B3%D8%B4%D9%88%DB%8C%DB%8C%20%D9%85%D8%A7%D8%B4%DB%8C%D9%86%DB%8C%20500%20%DA%AF%D8%B1%D9%85%DB%8C&price=5355&discount=10&sku=AC696OT084RNIALIYUN&isavailable=true&category=%D8%B3%D9%88%D9%BE%D8%B1%D9%85%D8%A7%D8%B1%DA%A9%D8%AA%2C%D8%A8%D9%87%D8%AF%D8%A7%D8%B4%D8%AA%20%D9%85%D9%86%D8%B2%D9%84%2C%D8%B4%D9%88%DB%8C%D9%86%D8%AF%D9%87%20%D9%84%D8%A8%D8%A7%D8%B3&brand=Active
 func getAsset(ctx context.Context, w http.ResponseWriter, r *http.Request) {
-
 	var err error
-
 	var msg = ""
-
 	defer func() {
-
 		if err != nil {
-
 			w.Header().Set("error", msg)
-
 			_, _ = w.Write([]byte(msg))
-
 			w.WriteHeader(http.StatusInternalServerError)
-
 		}
-
 	}()
 
 	pl := &item.Asset{}
-
 	u, ok := ctx.Value(user.KEY).(*openrtb.User)
-
 	if !ok {
-
 		err = fmt.Errorf("user not found")
 		msg = err.Error()
 		return
 	}
 	pl.User = u
-
 	ul, err := url.Parse(r.URL.Query().Get("url"))
 	if err != nil {
-		msg = "list does not exists"
+		msg = "invalid url"
 		return
 	}
 	ti := r.URL.Query().Get("title")
@@ -121,14 +108,14 @@ func getAsset(ctx context.Context, w http.ResponseWriter, r *http.Request) {
 	pl.FSKU = r.URL.Query().Get("sku")
 	pl.FBrand = r.URL.Query().Get("brand")
 
-	// go func() {
-	err = lists.SetLists(ctx, u.Id, pl.Hash(), l.KEY)
-	fmt.Println("ASSET", u.Id, pl.Hash(), l.KEY)
-	if err != nil {
-		xlog.GetWithError(ctx, err).Debug("set list id")
-	}
-	assetChan <- pl
-	// }()
+	go func() {
+		err = lists.SetLists(ctx, u.Id, pl.Hash(), l.KEY)
+		fmt.Println("ASSET", u.Id, pl.Hash(), l.KEY)
+		if err != nil {
+			xlog.GetWithError(ctx, err).Debug("set list id")
+		}
+		assetChan <- pl
+	}()
 }
 
 func init() {
